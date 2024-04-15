c24d4c34629dc40283496a92d7ecae2c17e32248
Hasan Furkan Gonulal <f.gonulal@Dev-PC.local>
2024-04-15 11:16:40 +0300

    crm added to index

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const Books = require("./books/index");
 const Expense = require("./expense/index");
 const Desk = require("./desk/index");
 const Zoho = require("./zoho/auth.zoho");
-
+const CRM = require("./crm/index");
 module.exports = {
-    People,
-    QNTRL,
-    Orchestly,
-    Books,
-    Expense,
-    Desk,
-    Zoho
+  People,
+  QNTRL,
+  Orchestly,
+  Books,
+  Expense,
+  Desk,
+  Zoho,
+  CRM,
 };
